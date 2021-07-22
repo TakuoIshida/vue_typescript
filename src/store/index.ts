7ba9f8a26ab872425ac763dab36eede78cdfd8bc
TakuoIshida <archersparadox.100@gmail.com>
2021-07-22 12:25:37 +0900

    store、mutasion周り実装

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,27 @@
-import Vue from "vue";
-import Vuex from "vuex";
-
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { mTypes } from './mutation-types';
 Vue.use(Vuex);
 
+// Stateの型を定義しておく
+export type State = {
+  storeCount: number;
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  // 実際保持されるState
+  state: {
+    storeCount: 0, //初期値
+  },
+  mutations: {
+    //※コンポネントからは、関数名を文字列で指定するので、定数の型で定義してから使用する
+    [mTypes.COUNT_UP](state: State, payload) {
+      state.storeCount = payload.storeCount + 1;
+    },
+    [mTypes.COUNT_DOWN](state: State, payload: { storeCount: number }) {
+      state.storeCount = payload.storeCount - 1;
+    },
+  },
   actions: {},
-  modules: {}
+  modules: {},
 });
---
+++ src/store/mutation-types.ts
@@ -0,0 +1,4 @@
+export const mTypes = {
+  COUNT_UP: 'COUNT_UP',
+  COUNT_DOWN: 'COUNT_DOWN',
+} as const;
