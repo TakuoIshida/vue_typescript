59f5b1cb9b5bcbb64d8f4b700deb123878f20cc9
TakuoIshida <archersparadox.100@gmail.com>
2021-07-22 12:26:12 +0900

    data, storeのカウンターをセット

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,23 +4,49 @@
     <!-- 写真など大きい静的ファイルはpublic, 小さい画像はassetsに。 -->
     <img alt="Vue logo" src="../assets/logo.png" />
     <!-- msg をpropsで値を渡している -->
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div>dataのCount：{{dataCount}}</div>
+    <div>storeのCount：{{getStoreCount}}</div>
+    <button @click="countUpData">dataCount ++</button>
+    <button @click="countDownData">dataCount --</button>
+    <button @click="countUpStore">storeCount ++</button>
+    <button @click="countDownStore">storeCount --</button>
   </div>
 </template>
 
-
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; 
-// @ is an alias to /src
-// @ で、/srcからのimportをかける。※統一すること
+import Vue from "vue";
+import {State} from "../store"
+import {mTypes} from '../store/mutation-types'
 
-// 上のtemplateで使用するコンポネントを宣言する
-@Component({
-  components: {
-    HelloWorld
+type DataType = {
+  dataCount: number;
+};
+export default Vue.extend({
+  data(): DataType {
+    return {
+      dataCount: 0,
+    };
+  },
+  computed: {
+    getStoreCount(): number {
+    console.log(this.$store.state);
+    // Vueは、storeに対して型補完が効かない（相性が悪い）ため、自作したStateで型キャストする
+    return (this.$store.state as State).storeCount
+    }
+  },
+  methods: {
+    countUpData(): void {
+      this.dataCount++
+    },
+    countDownData(): void {
+      this.dataCount--
+    },
+    countUpStore(): void {
+      this.$store.commit(mTypes.COUNT_UP, {storeCount: this.getStoreCount})
+    },
+    countDownStore(): void {
+      this.$store.commit(mTypes.COUNT_DOWN, {storeCount: this.getStoreCount})
+    },
   }
-})
-// Vue本体を継承したHomeコンポネントを外部にexportする
-export default class Home extends Vue {}
+});
 </script>
